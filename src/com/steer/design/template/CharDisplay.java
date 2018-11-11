f86409f016e5d64d181f9f6eb0f596f81ebc9035
steer <565355716@qq.com>
2018-11-11 22:36:05 +0800

    init 模板模式

---
+++ src/com/steer/design/template/CharDisplay.java
@@ -0,0 +1,24 @@
+package com.steer.design.template;
+
+public class CharDisplay extends AbstractDisplay{
+    private char ch;
+
+    public CharDisplay(char ch) {
+        this.ch = ch;
+    }
+
+    @Override
+    public void open() {
+        System.out.println("<<<");
+    }
+
+    @Override
+    public void print() {
+        System.out.println(ch);
+    }
+
+    @Override
+    public void close() {
+        System.out.println(">>>");
+    }
+}
