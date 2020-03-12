7d63c457498920403fdb98b05ce0021f80aa02f8
steer <565355716@qq.com>
2020-03-12 22:04:42 +0800

    volatile 指令重排

--- src/com/steer/design/singleton/two/Student.java
+++ src/com/steer/design/singleton/two/Student.java
@@ -2,6 +2,9 @@ package com.steer.design.singleton.two;
 
 public class Student {
     private static final Object obj = new Object();
+    /**
+     * 必须要加volatile,可以防止指令重排
+     */
     private volatile static Student student = null;
 
     private Student() {
@@ -12,6 +15,15 @@ public class Student {
         if (student == null) {
             synchronized (obj){
                 if (student == null)
+                /**
+                 * jvm指令分3部
+                 * 1.alloc分配内存
+                 * 2.初始化赋值
+                 * 3.student指向分配空间
+                 *
+                 * 2，3步骤替换不会改变最后执行结果，所以可能会发生指令重排。
+                 * 导致其他线程等到的student不为空，但未初始化值的对象，直接return了
+                 */
                     student = new Student();
             }
         }
